df5e3ef14d6b4a7757b2d554ac61380c47c08bda
Ubuntu <ubuntu@ip-172-31-35-183.us-east-2.compute.internal>
2020-02-07 16:33:52 +0000

    submission

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'quizzes'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:quiz_id>/', views.detail, name='detail')
 ]
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
+from .models import Quiz
 
 def index(request):
-    return HttpResponse("xxx")
+    quiz_list = Quiz.objects.order_by('quiz_title')
+    context = {'quiz_list':quiz_list}
+    return render(request, 'quizzes/index.html', context)
+
+def detail(request, quiz_id):
+    quiz = get_object_or_404(Quiz, pk=quiz_id)
+    return render(request, 'quizzes/detail.html', {'quiz': quiz})
