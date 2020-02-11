b7889013abcb3c7b822894615b636adef5c51dc1
Ubuntu <ubuntu@ip-172-31-35-183.us-east-2.compute.internal>
2020-02-11 19:13:12 +0000

    url config & page title modified

--- quizproject/urls.py
+++ quizproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quizzes/', include('quizzes.urls'))
+    path('', include('quizzes.urls'))
 ]
